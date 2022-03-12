a7ccf2c45acf8ce549eedbe0177374d41c6d7625
Mohammed Qaim Abbas <101219974+MQABBAS@users.noreply.github.com>
2022-03-12 22:56:52 +0000

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,25 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+if nr_letters > 8:
+    print('You Have A Good Range Of Letters In your Password.')
+else:
+    print('You must have at least 9 Characters in your password.')
+
+    if nr_symbols < 3:
+        print('You cant have more than 3 Symbols in your password.')
+    else:
+        print('Your password has a good range of symbols.')
+    
+    if nr_numbers > 4:
+        print('You have a good range of numbers in your password')
+    else:
+        print('You must have at least 5 numbers in your password. ')
